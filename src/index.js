905a1cf5de1733f0c2015a7773e8ca61c070104b
Daniel Olivares <daniel.olivares@parkhub.com>
2017-09-12 13:31:01 -0500

    feat(API): Added use and run
    The API now exposes use and run methods to add/execute middleware(respectively) This allows for more
    flexibility, control and potential for expansion. Middlewares all now receive a "next" function to
    their parameters.
    
    use and run are now the API and middlewares are no triggered by calling next

--- src/index.js
+++ src/index.js
@@ -5,84 +5,48 @@
  * @author Daniel Olivares
  */
 
-function last(arr) {
-  const length = arr.length;
+type Middleware = () => void;
+
+/*
+ * TODO The use method wraps an extra function around the stack of functions, how can we avoid it?
+*/
+function last(arr: mixed[]): mixed {
+  const { length } = arr;
 
   return arr[length - 1];
 }
 
-function reduceOneRight(arr) {
+function reduceOneRight(arr: mixed[]): mixed[] {
   return arr.slice(0, -1);
 }
 
-export default function circeMiddleware() {
-  const middleware = {
+export default function circeMiddleware(middlewares: Middleware[] = []) {
+  const middlewareProto: { use: (() => void) => void, run: () => void } = {
     use(fn) {
       this.run = (stack => (...args) => {
-        console.log('args', args);
-        const newStackArgs = reduceOneRight(args);
+        const nonNextArgs = reduceOneRight(args);
         const next = last(args);
 
-        // const wrappedNext = (...nextArgs) => {
-        //   stack(...nextArgs, next.bind.apply(next, [null, ...nextArgs]));
-        // };
-
-        // return fn(...newStackArgs, wrappedNext);
-        return stack(...newStackArgs, (...otherArgs) => {
-          console.log('other', otherArgs);
-          fn.apply(this, [...newStackArgs, next.bind.apply(next, [null, ...newStackArgs])]);
+        return stack(...nonNextArgs, (...nextArgs) => {
+          fn(...nextArgs, next);
         });
       })(this.run);
     },
 
     run(...args) {
-      console.log('running');
       const next = last(args);
+
+      if (typeof next !== 'function') {
+        throw new Error('Final paramater must be a function');
+      }
 
       next.apply(this, reduceOneRight(args));
     }
   };
 
-  return Object.create(middleware);
-}
-// function shouldShortCircuit(result) {
-//   if (typeof result === 'object' && result !== null) {
-//     return result.circeShortCircuit;
-//   }
-//
-//   return false;
-// }
+  const middleware = Object.create(middlewareProto);
 
-// export default function circeMiddleware(middlewareFns: Array<(mixed) => mixed>): mixed => mixed {
-//   const localMiddlewareFns = middlewareFns;
-//
-//   function applyMiddleware(...args: Array<mixed>) {
-//     const length = localMiddlewareFns.length;
-//
-//     let index = 0;
-//     let shortCircuit = false;
-//     let result = middlewareFns[index](...args);
-//
-//     // Evaluate initial result
-//     shortCircuit = shouldShortCircuit(result);
-//
-//     // Let's start it off with the next function
-//     index += 1;
-//
-//     while (index < length && !shortCircuit) {
-//       result = middlewareFns[index](result);
-//
-//       shortCircuit = shouldShortCircuit(result);
-//
-//       index += 1; // Keep the loop going since our next function has executed
-//     }
-//
-//     return result;
-//   }
-//
-//   applyMiddleware.addMiddleware = function addMiddleware(middlewareFn) {
-//     localMiddlewareFns.push(middlewareFn);
-//   };
-//
-//   return applyMiddleware;
-// }
+  middlewares.forEach(ware => middleware.use(ware));
+
+  return middleware;
+}
--- src/index.test.js
+++ src/index.test.js
@@ -1,9 +1,14 @@
 import circeMiddleware from './';
 
-test('Should get multiple middleware and execute them in order passing results to one another', () => {
-  expect.assertions(9);
-
+test('Should throw an error if the last parameter of a middleware is not a function', () => {
   const middleware = circeMiddleware();
+
+  expect(() => middleware.run()).toThrow();
+});
+
+test('Should accept an array of middlewares', () => {
+  expect.assertions(10);
+
   const startingValue = {
     test: 'one'
   };
@@ -13,7 +18,6 @@ test('Should get multiple middleware and execute them in order passing results t
   const finalReturn = 'final';
 
   const middlewareThree = jest.fn((value, next) => {
-    console.log('middleware three');
     expect(value).toEqual(middlewareTwoReturn);
 
     // eslint-disable-next-line no-use-before-define
@@ -26,201 +30,123 @@ test('Should get multiple middleware and execute them in order passing results t
   });
 
   const middlewareTwo = jest.fn((value, next) => {
-    console.log('middleware two', value);
-    // expect(value).toEqual(middlewareOneReturn);
-    // expect(middlewareThree).toHaveBeenCalledTimes(0);
+    expect(value).toEqual(middlewareOneReturn);
+    expect(middlewareThree).toHaveBeenCalledTimes(0);
 
     // eslint-disable-next-line no-use-before-define
-    // expect(middlewareOne).toHaveBeenCalledTimes(1);
+    expect(middlewareOne).toHaveBeenCalledTimes(1);
 
-    console.log('next there', JSON.stringify(next));
     return next(middlewareTwoReturn);
   });
 
   const middlewareOne = jest.fn((value, next) => {
-    console.log('middleware one', value);
-    // expect(value).toEqual(startingValue);
-    // expect(middlewareThree).toHaveBeenCalledTimes(0);
-    // expect(middlewareTwo).toHaveBeenCalledTimes(0);
+    expect(value).toEqual(startingValue);
+    expect(middlewareThree).toHaveBeenCalledTimes(0);
+    expect(middlewareTwo).toHaveBeenCalledTimes(0);
+
+    return next(middlewareOneReturn);
+  });
+
+  const middleware = circeMiddleware([middlewareOne, middlewareTwo]);
+  middleware.use(middlewareThree);
+
+  middleware.run(startingValue, (finalValue) => {
+    expect(finalValue).toBe(finalReturn);
+  });
+});
+
+test('Should get multiple middleware and execute them in order passing results to one another', () => {
+  expect.assertions(10);
+
+  const middleware = circeMiddleware();
+  const startingValue = {
+    test: 'one'
+  };
+
+  const middlewareOneReturn = 'string';
+  const middlewareTwoReturn = { batman: 'is cool' };
+  const finalReturn = 'final';
+
+  const middlewareThree = jest.fn((value, next) => {
+    expect(value).toEqual(middlewareTwoReturn);
+
+    // eslint-disable-next-line no-use-before-define
+    expect(middlewareTwo).toHaveBeenCalledTimes(1);
+
+    // eslint-disable-next-line no-use-before-define
+    expect(middlewareOne).toHaveBeenCalledTimes(1);
+
+    return next(finalReturn);
+  });
+
+  const middlewareTwo = jest.fn((value, next) => {
+    expect(value).toEqual(middlewareOneReturn);
+    expect(middlewareThree).toHaveBeenCalledTimes(0);
+
+    // eslint-disable-next-line no-use-before-define
+    expect(middlewareOne).toHaveBeenCalledTimes(1);
+
+    return next(middlewareTwoReturn);
+  });
+
+  const middlewareOne = jest.fn((value, next) => {
+    expect(value).toEqual(startingValue);
+    expect(middlewareThree).toHaveBeenCalledTimes(0);
+    expect(middlewareTwo).toHaveBeenCalledTimes(0);
 
     return next(middlewareOneReturn);
   });
 
   middleware.use(middlewareOne);
   middleware.use(middlewareTwo);
-  // middleware.use(middlewareThree);
+  middleware.use(middlewareThree);
 
-  console.log('run', middleware.run);
   middleware.run(startingValue, (finalValue) => {
-    console.log('final value', finalValue);
     expect(finalValue).toBe(finalReturn);
   });
 });
 
-// test('Should stop execution if any middleware throws', () => {
-//   expect.assertions(8);
-//
-//   const startingValue = {
-//     test: 'one'
-//   };
-//
-//   const middlewareOneReturn = 'string';
-//
-//   const middlewareThree = jest.fn();
-//
-//   const middlewareTwo = jest.fn((value) => {
-//     expect(value).toEqual(middlewareOneReturn);
-//     expect(middlewareThree).toHaveBeenCalledTimes(0);
-//
-//     // eslint-disable-next-line no-use-before-define
-//     expect(middlewareOne).toHaveBeenCalledTimes(1);
-//
-//     throw new Error('throwing');
-//   });
-//
-//   const middlewareOne = jest.fn((value) => {
-//     expect(value).toEqual(startingValue);
-//     expect(middlewareThree).toHaveBeenCalledTimes(0);
-//     expect(middlewareTwo).toHaveBeenCalledTimes(0);
-//
-//     return middlewareOneReturn;
-//   });
-//
-//   expect(() =>
-//     middleware([middlewareOne, middlewareTwo, middlewareThree])(startingValue)
-//   ).toThrow();
-//   expect(middlewareThree).toHaveBeenCalledTimes(0);
-// });
-//
-// test('Should allow me to add a middleware at the end of an existing set', () => {
-//   expect.assertions(9);
-//
-//   const startingValue = {
-//     test: 'one'
-//   };
-//
-//   const middlewareOneReturn = 'string';
-//   const middlewareTwoReturn = { batman: 'is cool' };
-//   const finalReturn = 'final';
-//
-//   const middlewareThree = jest.fn((value) => {
-//     expect(value).toEqual(middlewareTwoReturn);
-//
-//     // eslint-disable-next-line no-use-before-define
-//     expect(middlewareTwo).toHaveBeenCalledTimes(1);
-//
-//     // eslint-disable-next-line no-use-before-define
-//     expect(middlewareOne).toHaveBeenCalledTimes(1);
-//
-//     return finalReturn;
-//   });
-//
-//   const middlewareTwo = jest.fn((value) => {
-//     expect(value).toEqual(middlewareOneReturn);
-//     expect(middlewareThree).toHaveBeenCalledTimes(0);
-//
-//     // eslint-disable-next-line no-use-before-define
-//     expect(middlewareOne).toHaveBeenCalledTimes(1);
-//
-//     return middlewareTwoReturn;
-//   });
-//
-//   const middlewareOne = jest.fn((value) => {
-//     expect(value).toEqual(startingValue);
-//     expect(middlewareThree).toHaveBeenCalledTimes(0);
-//     expect(middlewareTwo).toHaveBeenCalledTimes(0);
-//
-//     return middlewareOneReturn;
-//   });
-//
-//   const middlewares = middleware([middlewareOne, middlewareTwo]);
-//
-//   middlewares.addMiddleware(middlewareThree);
-//
-//   middlewares(startingValue);
-// });
-//
-// test('Should short-circuit middleware if shortCircuit function is defined', () => {
-//   const startingValue = {
-//     test: 'one'
-//   };
-//
-//   const middlewareTwo = jest.fn();
-//
-//   const middlewareOne = jest.fn(() => ({
-//     circeShortCircuit: true
-//   }));
-//
-//   const middlewares = middleware([middlewareOne, middlewareTwo]);
-//
-//   middlewares(startingValue);
-//
-//   expect(middlewareOne).toHaveBeenCalledTimes(1);
-//   expect(middlewareTwo).toHaveBeenCalledTimes(0);
-// });
-//
-// test('Should allow for errors thrown and returned in one function', () => {
-//   const startingValue = {
-//     test: 'one'
-//   };
-//
-//   const middlewareTwo = jest.fn();
-//
-//   const middlewareOne = jest.fn(() => {
-//     try {
-//       throw new Error('Err');
-//     } catch (e) {
-//       return {
-//         circeShortCircuit: true
-//       };
-//     }
-//   });
-//
-//   const middlewares = middleware([middlewareOne, middlewareTwo]);
-//
-//   middlewares(startingValue);
-//
-//   expect(middlewareOne).toHaveBeenCalledTimes(1);
-//   expect(middlewareTwo).toHaveBeenCalledTimes(0);
-// });
-//
-// test('Should short-circuit from any middleware order', () => {
-//   const startingValue = {
-//     test: 'one'
-//   };
-//
-//   const middlewareFive = jest.fn();
-//   const middlewareFour = jest.fn();
-//   const middlewareThree = jest.fn(() => ({ circeShortCircuit: true }));
-//   const middlewareTwo = jest.fn();
-//   const middlewareOne = jest.fn();
-//
-//   const middlewares = middleware([
-//     middlewareOne,
-//     middlewareTwo,
-//     middlewareThree,
-//     middlewareFour,
-//     middlewareFive
-//   ]);
-//
-//   middlewares(startingValue);
-//
-//   expect(middlewareOne).toHaveBeenCalledTimes(1);
-//   expect(middlewareTwo).toHaveBeenCalledTimes(1);
-//   expect(middlewareThree).toHaveBeenCalledTimes(1);
-//   expect(middlewareFour).toHaveBeenCalledTimes(0);
-//   expect(middlewareFive).toHaveBeenCalledTimes(0);
-// });
-// const startingValue = {
-//   awesome: 'allura'
-// };
-//
-// const middlewareTwo = () => {};
-//
-// const middlewareOne = () => ({
-//   circeShortCircuit: true
-// });
-//
-// const middlewares = middleware([middlewareOne, middlewareTwo]);
-//
-// middlewares(startingValue);
+test('Should pass to each function regardless of arity', () => {
+  expect.assertions(9);
+
+  const middleware = circeMiddleware();
+  const startingValue = {
+    test: 'one'
+  };
+  const secondStartingValue = '2nd';
+
+  const firstmwareReturn = 'string';
+  const secondmwareReturn = { batman: 'is cool' };
+  const finalReturnOne = 'final';
+  const finalReturnTwo = 'finalTwo';
+
+  const middlewareThree = jest.fn(next => next(finalReturnOne, finalReturnTwo));
+
+  const middlewareTwo = jest.fn((middlewareOnePassedValue, middlewareTwoPassedValue, next) => {
+    expect(middlewareOnePassedValue).toEqual(firstmwareReturn);
+    expect(middlewareTwoPassedValue).toEqual(secondmwareReturn);
+
+    // eslint-disable-next-line no-use-before-define
+    expect(middlewareOne).toHaveBeenCalledTimes(1);
+
+    return next();
+  });
+
+  const middlewareOne = jest.fn((value, valueTwo, next) => {
+    expect(value).toEqual(startingValue);
+    expect(valueTwo).toEqual(secondStartingValue);
+    expect(middlewareTwo).toHaveBeenCalledTimes(0);
+
+    return next(firstmwareReturn, secondmwareReturn);
+  });
+
+  middleware.use(middlewareOne);
+  middleware.use(middlewareTwo);
+  middleware.use(middlewareThree);
+
+  middleware.run(startingValue, secondStartingValue, (finalValue, finalValueTwo) => {
+    expect(finalValue).toBe(finalReturnOne);
+    expect(finalValueTwo).toBe(finalReturnTwo);
+    expect(middlewareThree).toHaveBeenCalledTimes(1);
+  });
+});
